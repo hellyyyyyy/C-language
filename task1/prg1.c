d635b1cd4ec2942effe7a986addd7246d5dbd9d2
hellyyyyyy <134709785+hellyyyyyy@users.noreply.github.com>
2023-05-31 23:52:28 +0530

    Add files via upload

---
+++ task1/prg1.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+	printf("Name: HELLY\n");
+	printf("Age : 17\n");
+	printf("School: Ashadeep International School\n");
+	return 0;
+}
+#include<stdio.h>
+int main() {
+	printf("hhhhhhh");
+	return 0;
+}
---
+++ task1/prg3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+	printf(" - - -\n");
+	printf("|     |\n");
+	printf("H     |\n");
+	printf("E     |\n");
+	printf("L     |\n");
+	printf("L     |\n");
+	printf("Y     |\n");
+	printf("|     |\n");
+	printf(" - - -\n");
+		return 0;
+}
