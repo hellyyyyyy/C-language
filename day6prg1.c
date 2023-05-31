7cbaf718e8a038477c2fcd15815eaad45ab17a15
hellyyyyyy <134709785+hellyyyyyy@users.noreply.github.com>
2023-05-31 18:32:03 +0530

    Add files via upload

---
+++ day6prg1.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main(){
+	int x,y;
+	printf("enter value of x:");
+	scanf("%d",&x);
+	printf("enter value of y:");
+	scanf("%d",&y);
+	if(x>y){
+    printf("y is minimum");		
+	}
+	else{
+		printf("x is minimum");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
