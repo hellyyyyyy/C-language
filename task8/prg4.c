ca456602cb568e8874228737e9d89cd6c9e242da
hellyyyyyy <134709785+hellyyyyyy@users.noreply.github.com>
2023-06-05 19:09:08 +0530

    Add files via upload

---
+++ task8/prg4.c
@@ -0,0 +1,59 @@
+#include<stdio.h>
+int main(){
+	int a,b,c,d,e;
+	
+	printf("Enter A:");
+	scanf("%d",&a);
+	printf("Enter B:");
+	scanf("%d",&b);
+	printf("Enter C:");
+	scanf("%d",&c);
+	printf("Enter D:");
+	scanf("%d",&d);
+	printf("Enter E:");
+	scanf("%d",&e);
+	
+	a>b ?
+	a>c ?
+	a>d ?
+	a>e ? printf("A is max") 
+	:     printf("E is max")
+	:
+	d>b ?
+	d>c ?
+	d>e ? printf("D is max")
+	:     printf("E is max")
+	:
+	c>e ? printf("C is max")
+	:     printf("E is max")
+	:
+	b>e ? printf("B is max")
+	:     printf("E is max")
+	:
+	c>d ?
+   	c>e ? printf("C is max")
+	:     printf("E is max")
+	:
+	d>e ? printf("D is max")
+	:     printf("E is max")
+	:
+	b>c ?
+	b>d ?
+	b>e ? printf("B is max")
+	:     printf("E is max")
+	:
+	d>c ?
+	d>e ? printf("D is max")
+	:     printf("E is max")
+	:     
+	c>e ? printf("C is max")
+	:     printf("E is max")
+	:
+	c>d ?
+	d>e ? printf("D is max")
+	:     printf("E is max")
+	:
+	c>e ? printf("C is max")
+	:     printf("E is max");
+	return 0;
+}
