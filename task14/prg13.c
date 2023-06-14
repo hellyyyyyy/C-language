d97499fc2072b3dde042e0ef6084dc9399cae451
hellyyyyyy <134709785+hellyyyyyy@users.noreply.github.com>
2023-06-14 18:50:32 +0530

    Add files via upload

---
+++ task14/prg13.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main(){
+	char r,c,k;
+	
+	for(r='A';r<='E';r++)
+	
+	{
+		for(k='E';k>=r;k--){
+			printf(" ");
+		}
+		for(c=r;c>='A';c--){
+			printf("%c",c);
+		}
+		printf("\n");
+	}
+	
+	
+	return 0;
+}
